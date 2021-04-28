8f8acca2826d4f9adef6fcdffc236454047908f7
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-04-28 02:30:57 +0200

    feat: add target-applying esbuild plugin

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -3,25 +3,82 @@ import fs from 'fs';
 import os from 'os';
 import path from 'path';
 
+import browserslist from 'browserslist';
 import * as esbuild from 'esbuild';
 
 import { esbuildPluginBrowserslist } from '.';
 
+const getFile = async (
+  input: string,
+): Promise<{ entryPoint: string; outfile: string }> => {
+  const randomString = crypto.randomBytes(16).toString('hex');
+  const entryPoint = path.join(os.tmpdir(), `${randomString}.js`);
+  const outfile = path.join(os.tmpdir(), `${randomString}-out.js`);
+
+  await fs.promises.writeFile(entryPoint, input, 'utf8');
+
+  return { entryPoint, outfile };
+};
+
 describe('esbuild-plugin-browserslist', () => {
+  it('throws an error when a target is already set', async () => {
+    const { entryPoint, outfile } = await getFile('');
+
+    await expect(
+      esbuild.build({
+        target: 'node14',
+        entryPoints: [entryPoint],
+        outfile,
+        plugins: [
+          esbuildPluginBrowserslist([], {
+            printUnknownTargets: false,
+          }),
+        ],
+      }),
+    ).rejects.toThrow(/cannot be used with a set target/);
+  });
+
   it('builds correctly', async () => {
-    const randomString = crypto.randomBytes(16).toString('hex');
-    const entryPoint = path.join(os.tmpdir(), `${randomString}.js`);
-    const outfile = path.join(os.tmpdir(), `${randomString}-out.js`);
+    await Promise.all(
+      [
+        {
+          input: `const x = foo?.bar;`,
+          query: ['chrome 50'],
+          expectedOutput: `const x = foo == null ? void 0 : foo.bar;`,
+        },
+        {
+          input: `const x = foo?.bar;`,
+          query: ['node 14'],
+          expectedOutput: `const x = foo?.bar;`,
+        },
+        {
+          input: `const x = foo?.bar;`,
+          query: ['ios_saf 13.4-13.7'],
+          expectedOutput: `const x = foo?.bar;`,
+        },
+        {
+          input: `const x = foo?.bar;`,
+          // i.e. default
+          query: undefined,
+          expectedOutput: `const x = foo?.bar;`,
+        },
+      ].map(async ({ input, query, expectedOutput }) => {
+        const { entryPoint, outfile } = await getFile(input);
 
-    const input = `const x = foo?.bar;`;
-    await fs.promises.writeFile(entryPoint, input, 'utf8');
-    await esbuild.build({
-      entryPoints: [entryPoint],
-      outfile,
-      plugins: [esbuildPluginBrowserslist()],
-    });
+        await esbuild.build({
+          entryPoints: [entryPoint],
+          outfile,
+          plugins: [
+            esbuildPluginBrowserslist(browserslist(query), {
+              printUnknownTargets: false,
+            }),
+          ],
+        });
 
-    const expectedOutput = `const x = foo == null ? void 0 : foo.bar;\n`;
-    expect(await fs.promises.readFile(outfile, 'utf8')).toBe(expectedOutput);
+        expect(await fs.promises.readFile(outfile, 'utf8')).toBe(
+          `${expectedOutput}\n`,
+        );
+      }),
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,25 @@
 import type { Plugin } from 'esbuild';
 
-export const esbuildPluginBrowserslist = (): Plugin => ({
-  name: 'esbuild-plugin-browserslist',
-  setup(_build) {},
+import { resolveToEsbuildTarget } from './resolveToEsbuildTarget';
+import { dbg, log, PLUGIN_NAME } from './util';
+
+export const esbuildPluginBrowserslist = (
+  browserlist: string[],
+  opts: { printUnknownTargets: boolean },
+): Plugin => ({
+  name: PLUGIN_NAME,
+  setup(build) {
+    const printUnknownTargets = opts.printUnknownTargets ?? true;
+
+    const esbuildOptions = build.initialOptions;
+
+    if (esbuildOptions.target) {
+      throw new Error(`${PLUGIN_NAME} cannot be used with a set target`);
+    }
+
+    esbuildOptions.target = resolveToEsbuildTarget(
+      browserlist,
+      printUnknownTargets ? log : dbg,
+    ).map(({ target, version }) => `${target}${version}`);
+  },
 });
