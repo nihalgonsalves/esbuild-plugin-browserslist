f32bdba8f752abffa6c6a63d0d477f6918598839
Nihal Gonsalves <nihal@nihalgonsalves.com>
2021-04-28 02:45:39 +0200

    feat: expose resolveToEsbuildTarget

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
 import type { Plugin } from 'esbuild';
 
-import { resolveToEsbuildTarget } from './resolveToEsbuildTarget';
+import { resolveToEsbuildTarget as resolveToEsbuildTarget_ } from './resolveToEsbuildTarget';
 import { dbg, log, PLUGIN_NAME } from './util';
+
+export type Opts = { printUnknownTargets: boolean };
+
+export const resolveToEsbuildTarget = (
+  browserlist: string[],
+  opts?: Opts,
+): string[] => {
+  const printUnknownTargets = opts?.printUnknownTargets ?? true;
+
+  return resolveToEsbuildTarget_(
+    browserlist,
+    printUnknownTargets ? log : dbg,
+  ).map(({ target, version }) => `${target}${version}`);
+};
 
 export const esbuildPluginBrowserslist = (
   browserlist: string[],
-  opts: { printUnknownTargets: boolean },
+  opts?: Opts,
 ): Plugin => ({
   name: PLUGIN_NAME,
   setup(build) {
-    const printUnknownTargets = opts.printUnknownTargets ?? true;
-
     const esbuildOptions = build.initialOptions;
 
     if (esbuildOptions.target) {
       throw new Error(`${PLUGIN_NAME} cannot be used with a set target`);
     }
 
-    esbuildOptions.target = resolveToEsbuildTarget(
-      browserlist,
-      printUnknownTargets ? log : dbg,
-    ).map(({ target, version }) => `${target}${version}`);
+    esbuildOptions.target = resolveToEsbuildTarget(browserlist, opts);
   },
 });
