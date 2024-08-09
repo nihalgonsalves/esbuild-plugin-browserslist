40dddbfd65a9d4d511cf7ee8be0368939259b6ab
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-08-09 15:28:13 +0200

    chore(deps): update to eslint v9

--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,6 @@ import debug from "debug";
 export const PLUGIN_NAME = "esbuild-plugin-browserslist";
 
 export const log = (msg: string): void => {
-  // eslint-disable-next-line no-console
   console.error(`[${PLUGIN_NAME}] ${msg}`);
 };
 
