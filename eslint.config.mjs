c77f6478974a4635a6b76a01a82ecbd7d993920a
Nihal Gonsalves <nihal@nihalgonsalves.com>
2024-08-09 15:52:14 +0200

    chore(deps): update to eslint v9 (#205)

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { includeIgnoreFile } from "@eslint/compat";
+import vitest from "eslint-plugin-vitest";
+import tseslint from "typescript-eslint";
+
+// eslint-disable-next-line import/extensions
+import sharedConfig from "@nihalgonsalves/esconfig/eslint.config.shared.js";
+
+export default tseslint.config(
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  includeIgnoreFile(new URL("./.gitignore", import.meta.url).pathname),
+  { ignores: ["package-test", "dist"] },
+  ...sharedConfig,
+  {
+    plugins: { vitest },
+    rules: vitest.configs.recommended.rules,
+  },
+);
